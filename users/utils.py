f7e21309dd8093bbfd3011fab91143f41507859a
mullerjuninorte <75736282+mullerjuninorte@users.noreply.github.com>
2021-08-20 01:58:49 -0500

    Add files via upload

---
+++ users/utils.py
@@ -0,0 +1,14 @@
+'''//from pymongo import MongoClient
+client = pymongo.MongoClient('connection_string')
+db = client['db_name']
+
+connection_string = mongodb+srv://MisionTIC:pass123@cuentasdb.92hrm.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
+
+dbname = my_client['hojasdevida']
+collection object
+collection_name = dbname["usuarios"]
+documents
+usuario_1 = {
+    "nombre_usuario" : "Carlos",
+    "contraseña" : "123"
+}'''
