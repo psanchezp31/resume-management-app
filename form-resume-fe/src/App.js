90b6d65c012117e1db9152ae8177bb54b90db673
Paula Sánchez P <paula.sanchez1031@gmail.com>
2021-08-25 09:27:29 -0500

    Uploading form-resume-frontend file

---
+++ form-resume-fe/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Form from './components/Forms'
+import './App.css';
+
+function App() {
+  return (
+    <div className= "container mt-5">
+      <Form/>
+    </div>
+  );
+}
+
+export default App;
